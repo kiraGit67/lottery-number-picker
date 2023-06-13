1bb2e28f135ac20f68259ed1423903d73fc2a832
kiraGit67 <kirsten@sais.de>
2023-06-13 18:55:38 +0200

    removes obsolet comments from scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,5 @@
 "use strict";
 
-//import { randomIntFromInterval } from "./random-int-from-interval.js";
 import { pickUniqueNumber, lotteryNumbers } from "./pick-unique-number.js";
 import { resetLotteryNumbers } from "./reset-lottery-numbers.js";
 
@@ -10,9 +9,6 @@ const resetButton = document.querySelector("#reset-btn");
 
 //Variable for numbersContainer
 const numbersContainer = document.querySelector(".numbers");
-
-//Variable for Lottery Numbers Array
-//const lotteryNumbers = [];
 
 //Event Listener for pickNumberButton
 pickNumberButton.addEventListener("click", function () {
@@ -37,32 +33,9 @@ function renderLottoNumbers() {
     numbersContainer.appendChild(numberSpan);
   }
 }
-/*
-//Function to generate a unique number
-function pickUniqueNumber() {
-  let number = randomIntFromInterval(1, 49);
 
-  while (lotteryNumbers.includes(number) === true) {
-    number = randomIntFromInterval(1, 49);
-  }
-
-  return number;
-}
-
-
-//Function to generate a random Number between 1 and 49
-function randomIntFromInterval(min, max) {
-  // min and max included
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-*/
 //Event Listener for Reset Button
 resetButton.addEventListener("click", function () {
-  /*
-  if (lotteryNumbers.length === 6) {
-    lotteryNumbers.length = 0;
-  }
-  */
   resetLotteryNumbers(lotteryNumbers);
   renderLottoNumbers();
 });
