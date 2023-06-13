fcab2572c41af4325050204f4f0e8eb18c1e6bd1
kiraGit67 <kirsten@sais.de>
2023-06-13 16:30:58 +0200

    imports scripts/random-int-from-interval.js into scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import { randomIntFromInterval } from "./random-int-from-interval.js";
+
 //Variables for Buttons
 const pickNumberButton = document.querySelector("#pick-number-btn");
 const resetButton = document.querySelector("#reset-btn");
@@ -45,12 +47,13 @@ function pickUniqueNumber() {
   return number;
 }
 
+/*
 //Function to generate a random Number between 1 and 49
 function randomIntFromInterval(min, max) {
   // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
+*/
 //Event Listener for Reset Button
 resetButton.addEventListener("click", function () {
   if (lotteryNumbers.length === 6) {
